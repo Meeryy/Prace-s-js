999dc5b8ed599930cf43f9e7e9dfea2b3331251d
Marie Kapucianova <kapucianova@spsejecna.cz>
2023-11-15 08:46:41 +0100

    scripty a index

---
+++ script.js
@@ -0,0 +1,43 @@
+let a = 6;
+let b = 3;
+
+function plus(a,b){
+   return a+b;
+}
+function minus(a,b){
+   return a-b;
+}
+function nasobeni(a,b){
+    return a*b;
+}
+function deleni(a,b){
+    return a/b;
+}
+
+function sin(a){
+    return Math.sin(a);
+}
+
+function cos(a){
+    return Math.cos(a);
+}
+
+function tg(a){
+    Math.tan(a);
+}
+
+function cot(a){
+    Math.cot(a);
+}
+
+function log(a){
+    Math.log(a);
+}
+
+function exp(a){
+    Math.exp(a);
+}
+
+function sqr(a){
+    Math.sqr(a);
+}
